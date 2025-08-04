9b3d98ea56ea0d4cf9a712b21c4fcdd3d38db2b2
Akhi Azim Papri <akhi@Akhis-MacBook-Air.local>
2025-08-04 09:38:58 +0600

    first commit

---
+++ FCNN.py
@@ -0,0 +1,9 @@
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+
+inputs = Input((2,))
+h1 = Dense(4, activation = 'relu')(inputs)
+h2 = Dense(3, activation = 'relu')(h1)
+outputs = Dense(2, activation = 'softmax')(h2)
+model = Model(inputs, outputs)
+model.summary(show_trainable=True)
