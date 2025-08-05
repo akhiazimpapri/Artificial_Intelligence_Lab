f094f2bd727464cc7706080308a3e39bb905357d
Akhi Azim Papri <113493951+akhiazimpapri@users.noreply.github.com>
2025-08-05 23:35:19 +0600

    Update FCNN.py

--- FCNN.py
+++ FCNN.py
@@ -1,9 +1,9 @@
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 
-inputs = Input((2,))
+inputs = Input((1,))
 h1 = Dense(4, activation = 'relu')(inputs)
 h2 = Dense(3, activation = 'relu')(h1)
-outputs = Dense(2, activation = 'softmax')(h2)
+outputs = Dense(1, activation = 'softmax')(h2)
 model = Model(inputs, outputs)
 model.summary(show_trainable=True)
